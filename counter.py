02f30a646d30c739b645c00467bb7720953dfe59
Veliaraft <43182375+Veliaraft@users.noreply.github.com>
2021-12-08 10:16:15 +0800

    New comm
    Добавил отображение для каждого объекта отображение его типа и количества.

--- counter.py
+++ counter.py
@@ -15,6 +15,13 @@ image, quantity, masks = np.load('ps.npy'), 0, np.array([[[1,1,1,1],
     [0,0,1,1],
     [1,1,1,1],
     [1,1,1,1]],
+
+    [[1,1,1,1],
+    [1,1,1,1],
+    [1,1,1,1],
+    [1,1,1,1],
+    [1,1,1,1],
+    [1,1,1,1]],
     
     [[1,1,0,0,1,1],
     [1,1,0,0,1,1],
@@ -31,9 +38,19 @@ image, quantity, masks = np.load('ps.npy'), 0, np.array([[[1,1,1,1],
     [1,1,1,1,1,1],
     [1,1,1,1,1,1]]], dtype=object)
 
+def type_of_mask(j):
+    stringa = ''
+    for n in j[::2]:
+        stringa += str(n[::2]).replace('[', '').replace(']', '').replace(', ', '')
+        stringa += '\n'
+    return stringa
+
 for i in masks:
-    quantity += np.sum(morphology.binary_hit_or_miss(image, i))
+    result = np.sum(morphology.binary_hit_or_miss(image, i))
+    print('Объекты типа: \n', type_of_mask(i), 'Их количество = ', result, sep = '', end = '\n\n')
+    quantity += result
 print("Количество объектов на изображении: ", quantity)
+
 plt.figure()
 plt.imshow(image, cmap="gray")
 plt.show()
